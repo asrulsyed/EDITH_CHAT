e6afa70bc48a9d49c4d41a3ebc386a6eda1930c2
Asrul Syed <asrulzsyed@gmail.com>
2025-01-20 11:19:53 -0800

    fix

--- src/pages/Code.tsx
+++ src/pages/Code.tsx
@@ -1,4 +1,4 @@
-import { useAuth } from "@/context/AuthContext";
+// import { useAuth } from "@/context/AuthContext";
 import { toast } from "@/hooks/use-toast";
 import { AuthError } from "@/lib/types";
 import {
